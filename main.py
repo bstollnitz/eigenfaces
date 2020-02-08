a674dc17ddab30ad3edcd56713444f70f0fdbb2f
Bea Stollnitz <bea@stollnitz.com>
2020-02-08 11:39:13 -0800

    Project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+S3_URL = 'https://bea-portfolio.s3-us-west-2.amazonaws.com/eigenfaces/'
+
+def main() -> None:
+    """Main program.
+    """
+    print("Main eigenfaces.")
+
+if __name__ == '__main__':
+    main()
